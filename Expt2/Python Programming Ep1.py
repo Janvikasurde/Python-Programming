189c49743f676ac9007c7d143ed32a650c86e739
Janvi Kasurde <janvikasurde18@gmail.com>
2026-03-12 10:22:09 +0530

    Add files via upload

---
+++ Expt2/Python Programming Ep1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  5 10:54:30 2026
+
+@author: User
+"""
+
+name=input("enter you name:")
+age=input("enter your age:")
+roll_no=input("enter your roll no:")
+branch=input("enter your branch:")
+print("name:",name)
+print("age:",age)
+print("roll_no", roll_no)
+print("branch",branch)
