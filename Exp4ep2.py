9e8da0107670b213e0364c87cf05b266b6157f42
@janvikasurde <janvikasurde18@gmail.com>
2026-04-02 10:36:55 +0530

    Add files via upload

---
+++ Exp4ep2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 10:21:39 2026
+
+@author: User
+"""
+
+# Take input from the user
+string = input("Enter a string: ")
+# Reverse the string using slicing
+reversed_string = string[::-1]
+# Print the result
+print("Reversed string:", reversed_string)
